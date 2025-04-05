255aab15832d9c822238edab7575dd6d4aa2794d
Luc Martial <lucmartial@msn.com>
2025-04-05 18:04:40 +0200

    JS-Monorepo

---
+++ Monorepo/server/src/router.ts
@@ -0,0 +1,52 @@
+import express from "express";
+import categoryActions from "./modules/category/categoryActions";
+import itemActions from "./modules/item/itemActions";
+import programActions from "./modules/program/programActions";
+import sayActions from "./modules/say/sayActions";
+
+const router = express.Router();
+
+/* ************************************************************************* */
+// Define Your API Routes Here
+/* ************************************************************************* */
+
+// Define item-related routes
+router.get("/api/items", itemActions.browse);
+router.get("/api/items/:id", itemActions.read);
+router.post("/api/items", itemActions.add);
+
+// Define program-related routes
+router.get("/api/programs", programActions.browse);
+router.get("/api/programs/:id", programActions.read);
+router.put(
+  "/api/programs/:id",
+
+  programActions.validate,
+  programActions.edit,
+);
+
+router.post("/api/programs", programActions.validate, programActions.add);
+router.delete("/api/programs/:id", programActions.destroy);
+
+// Define category-related routes
+router.get("/api/categories", categoryActions.browse);
+router.get("/api/categories/:id", categoryActions.read);
+router.put(
+  "/api/categories/:id",
+
+  categoryActions.validate,
+
+  categoryActions.edit,
+);
+
+router.post("/api/categories", categoryActions.validate, categoryActions.add);
+router.delete("/api/categories/:id", categoryActions.destroy);
+
+/* ************************************************************************* */
+
+// Declaration of a "Welcome" route
+router.get("/", sayActions.sayWelcome);
+
+/* ************************************************************************* */
+
+export default router;
